a3a9df906e5ffa8d6f109aaf212553cc1a72f0cb
Shaha-Alam <shahaalam3002@gmail.com>
2022-05-10 19:03:29 +0600

    last commit

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const toggleSpin = displaySpinner => {
     spinner.style.display = displaySpinner;
 }
 
-
 //search btn function 
 search.addEventListener('click', () => {
     const inputValue = input.value;
@@ -42,6 +41,7 @@ const morePhones = (phoneName) => {
 
 }
 
+
 // getting data from api for phone brand 
 const onLoad = (brandName) => {
     input.value = '';
@@ -50,4 +50,98 @@ const onLoad = (brandName) => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhone(data))
+}
+
+
+// display phone brand and name 
+const displayPhone = (spec) => {
+    console.log(spec.data);
+    cartDiv.textContent = '';
+    let phoneSummary = spec.data;
+    if (phoneSummary.length === 0) {
+        showMoreBtn.style.display = 'none';
+        alert('Not Available in our store');
+    } else {
+        phoneValue = phoneSummary[0].brand;
+        if ((phoneSummary.length > 20) && showLess) {
+            console.log(phoneSummary.length);
+            phoneSummary = phoneSummary.slice(0, 20);
+            showMoreBtn.style.display = 'block';
+        }
+
+        console.log(phoneSummary);
+        phoneSummary.forEach(brand => {
+            const phoneCard = document.createElement('div');
+            phoneCard.innerHTML = `     
+                    <div class="card text-center mb-3">
+                        <div class="card-body">
+                            <img src="${brand.image}" class="card-img-top w-50" alt="...">
+                            <h5 class="card-title">${brand.phone_name}</h5>
+                      
+                            <button onclick="phoneId('${brand.slug}')" type="button" class="btn btn-primary">Details</button>
+                        </div>
+                    </div>
+             
+        `;
+
+            cartDiv.appendChild(phoneCard);
+
+        })
+    }
+    toggleSpin('none');
+    detailsDiv.textContent = '';
+    showLess = true;
+
+}
+
+// search by phone id 
+const phoneId = id => {
+    detailsDiv.textContent = '';
+    window.scrollTo(0, 5000);
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    toggleSpin('block');
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data.data))
+}
+
+
+// display phone details 
+const displayDetails = details => {
+
+        const sensors = details.mainFeatures.sensors;
+        console.log(sensors);
+        const others = details.others;
+        phoneValue = details.brand;
+        console.log(phoneValue);
+        const specificationDiv = document.createElement('div');
+        specificationDiv.innerHTML = `
+
+    <div class="card text-center mb-3">
+        <div class="card-body" >
+          <h2 class="card-title my-3 text-primary">${details.name}</h2>
+          <img src="${details.image}" class="card-img-top w-25" alt="...">
+          <h4 class="card-text my-3 text-primary">Release Date: </h4><p>${details.releaseDate ? details.releaseDate:'Not available'}</p>
+          <h4 class="card-text my-3 text-primary">Memory: </h4><p>${details.mainFeatures.memory}</p>
+          <h4 class="card-text my-3 text-primary">Sensor: </h4><p>${(function loopSensor(sensors){
+              let sensor= '';
+              for(let i=0;i<sensors.length;i++){   
+                   sensor =sensor+ sensors[i]+' , ';
+              }
+              return sensor;
+          })(sensors)}</p>
+          <h4 class="card-text my-3 text-primary">Others: </h4><p>${others!=null ? (function loopOthers(other){
+              let property='';
+              for(let value in other){
+                  property+=`${value} : ${other[value]}`;
+              }
+              return property;
+          })(others): 'Not Available'}</p>
+        </div>
+    </div>
+      `;
+    
+    detailsDiv.appendChild(specificationDiv);
+    toggleSpin('none');
+
 }
